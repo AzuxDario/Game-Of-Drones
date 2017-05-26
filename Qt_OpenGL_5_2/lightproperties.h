d307b9c14eb298dcde5f4d471c2cbca9d5783ba3
AzuxDario <pawel_mibor95@tlen.pl>
2017-05-26 14:33:24 +0200

    Make LightProperties more C++14
    Add noexcept parameters, becaues that functions don't throw exceptions
    to caller.

--- Qt_OpenGL_5_2/lightproperties.h
+++ Qt_OpenGL_5_2/lightproperties.h
@@ -20,23 +20,23 @@ private:
 public:
 
     LightProperties();
-    QColor getAmbientColor() {return ambientColor;}
-    QColor getDiffuseColor() {return diffuseColor;}
-    QColor getSpecularColor() {return specularColor;}
-    GLFloat getAmbientReflection() {return ambientReflection;}
-    GLFloat getDiffuseReflection() {return diffuseReflection;}
-    GLFloat getSpecularReflection() {return specularReflection;}
-    GLFloat getShininess() {return shininess;}
-    void setAmbientColor(QColor color) {ambientColor = color;}
-    void setAmbientColor(int r, int g, int b, int a = 255) {ambientColor = QColor(r, g, b, a);}
-    void setDiffuseColor(QColor color) {diffuseColor = color;}
-    void setDiffuseColor(int r, int g, int b, int a = 255) {diffuseColor = QColor(r, g, b, a);}
-    void setSpecularColor(QColor color) {specularColor = color;}
-    void setSpecularColor(int r, int g, int b, int a = 255) {specularColor = QColor(r, g, b, a);}
-    void setAmbientReflection(GLFloat value) {ambientReflection = value;}
-    void setDiffuseReflection(GLFloat value) {diffuseReflection = value;}
-    void setSpecularReflection(GLFloat value) {specularReflection = value;}
-    void setShininess(GLFloat value) {shininess = value;}
+    QColor getAmbientColor() noexcept {return ambientColor;}
+    QColor getDiffuseColor() noexcept {return diffuseColor;}
+    QColor getSpecularColor() noexcept {return specularColor;}
+    GLFloat getAmbientReflection() noexcept {return ambientReflection;}
+    GLFloat getDiffuseReflection() noexcept {return diffuseReflection;}
+    GLFloat getSpecularReflection() noexcept {return specularReflection;}
+    GLFloat getShininess() noexcept {return shininess;}
+    void setAmbientColor(QColor color) noexcept {ambientColor = color;}
+    void setAmbientColor(int r, int g, int b, int a = 255) noexcept {ambientColor = QColor(r, g, b, a);}
+    void setDiffuseColor(QColor color) noexcept {diffuseColor = color;}
+    void setDiffuseColor(int r, int g, int b, int a = 255) noexcept {diffuseColor = QColor(r, g, b, a);}
+    void setSpecularColor(QColor color) noexcept {specularColor = color;}
+    void setSpecularColor(int r, int g, int b, int a = 255) noexcept {specularColor = QColor(r, g, b, a);}
+    void setAmbientReflection(GLFloat value) noexcept {ambientReflection = value;}
+    void setDiffuseReflection(GLFloat value) noexcept {diffuseReflection = value;}
+    void setSpecularReflection(GLFloat value) noexcept {specularReflection = value;}
+    void setShininess(GLFloat value) noexcept {shininess = value;}
 };
 
 #endif // LIGHTPROPERTIES_H
