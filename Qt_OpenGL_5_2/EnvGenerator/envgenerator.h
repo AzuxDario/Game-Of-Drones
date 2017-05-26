c1eb40aa5bdd609e68810119964f5bb0f61f3e43
AzuxDario <pawel_mibor95@tlen.pl>
2017-05-26 14:51:59 +0200

    Refactorina EnvGenerator

--- Qt_OpenGL_5_2/EnvGenerator/envgenerator.h
+++ Qt_OpenGL_5_2/EnvGenerator/envgenerator.h
@@ -10,19 +10,7 @@
 
 class EnvGenerator
 {
-public:
-    EnvGenerator();
-    ~EnvGenerator();
-
-    void Init(OBJManager* objManager, TexturesManager* texturesManager, QOpenGLShaderProgram* shader);
-    void Logic(QVector3D playerPosition, int deltaTime);
-    void Draw(Camera camera, Light light, QMatrix4x4 pMatrix);
-
-    QVector<DrawableObject*> GetObjects();
-    void RemoveObject(DrawableObject* object);
-    void RemoveObjects(QVector<DrawableObject*> objectsToRemove);
 private:
-    float GetRandomNumberWithNegatives(float to);
 
     OBJManager* objManager;
     TexturesManager* texturesManager;
@@ -33,6 +21,23 @@ private:
     int maxPlanetoidsCount;
     float maxPlanetoidsRotationSpeed;
     float maxPlanetoidsMoveSpeed;
+
+public:
+
+    EnvGenerator();
+    ~EnvGenerator();
+
+    void Init(OBJManager* objManager, TexturesManager* texturesManager, QOpenGLShaderProgram* shader);
+    void Logic(QVector3D playerPosition, int deltaTime);
+    void Draw(Camera camera, Light light, QMatrix4x4 pMatrix);
+
+    QVector<DrawableObject*> GetObjects();
+    void RemoveObject(DrawableObject* object);
+    void RemoveObjects(QVector<DrawableObject*> objectsToRemove);
+
+private:
+    float GetRandomNumberWithNegatives(float to);
+
 };
 
 #endif // ENVGENERATOR_H
