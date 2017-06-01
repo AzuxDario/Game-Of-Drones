6a999d4a7e43a70da2a6c62f901a806e60e97df3
AzuxDario <pawel_mibor95@tlen.pl>
2017-06-01 22:00:52 +0200

    Mikro refactor

--- Qt_OpenGL_5_2/lightproperties.cpp
+++ Qt_OpenGL_5_2/lightproperties.cpp
@@ -2,9 +2,9 @@
 
 LightProperties::LightProperties()
 {
-    ambientColor = QColor(32, 32, 32);
-    diffuseColor = QColor(128, 128, 128);
-    specularColor = QColor(255, 255, 255);
+    ambientColor.setRgb(32,32,32);
+    diffuseColor.setRgb(128, 128, 128);
+    specularColor.setRgb(255, 255, 255);
     ambientReflection = 1;
     diffuseReflection = 1;
     specularReflection = 1;
